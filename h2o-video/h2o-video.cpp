2a80f31cd3ed084dca1a7b0475fbc185b99484b3
marinedalek <marinedalek@gmail.com>
2022-01-21 00:28:30 +0000

    Add project files.

---
+++ h2o-video/h2o-video.cpp
@@ -0,0 +1,12 @@
+﻿// h2o-video.cpp : Defines the entry point for the application.
+//
+
+#include "h2o-video.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ h2o-video/h2o-video.h
@@ -0,0 +1,8 @@
+﻿// h2o-video.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
