280892ad65914b48e64e110df34fec1c4f12a4a3
marinedalek <marinedalek@gmail.com>
2022-01-22 03:52:28 +0000

    Palette extraction (only works for extracted H2O from C20.AP)

--- h2o-video/h2o-video.cpp
+++ h2o-video/h2o-video.cpp
@@ -2,19 +2,4 @@
 
 #include "h2o-video.h"
 
-int main()
-{
-	int constexpr screen_width{ 800 };
-	int constexpr screen_height{ 600 };
 
-	InitWindow(screen_width, screen_height, "h2o-video");
-	SetTargetFPS(60);
-
-	while (!WindowShouldClose()) {
-		BeginDrawing();
-		ClearBackground(DARKGRAY);
-		EndDrawing();
-	}
-
-	return 0;
-}
--- h2o-video/h2o-video.h
+++ h2o-video/h2o-video.h
@@ -1,6 +1,19 @@
 ﻿#ifndef H2O_VIDEO_H_MARINEDALEK
 #define H2O_VIDEO_H_MARINEDALEK
 #include <raylib.h> 
+#include <array>
+#include <cstdint>
 #include <iostream> 
 
+namespace h2o {
+struct Color_rgb {
+	uint8_t	r;
+	uint8_t g;
+	uint8_t b;
+};
+
+using Palette = std::array<Color_rgb, 256>;
+
+
+}
 #endif
